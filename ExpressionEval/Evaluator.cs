c4b7924fa7313c81f3779f106584cf01a2c31955
Lee Fogg <leefogg@users.noreply.github.com>
2018-02-15 12:31:15 +0000

    Cleaned up and optimized

--- ExpressionEval/Evaluator.cs
+++ ExpressionEval/Evaluator.cs
@@ -11,7 +11,7 @@ namespace ExpressionEval {
 			expression = expression.Replace(" ", "");
 			expression = $"({expression})";
 			int explength = 0;
-			var evaluationtree = buildExpression(0, expression, out explength);
+			var evaluationtree = buildExpression(1, expression, out explength);
 			return evaluationtree.value;
 		}
 
@@ -31,7 +31,7 @@ namespace ExpressionEval {
 					index = numberend;
 
 					nodes.Add(constant);
-				} else if (expression[index].isOperator() && index < expression.Length) {
+				} else if (expression[index].isOperator()) {
 					char op = expression[index];
 					var rightexpression = buildExpression(index + 1, expression, out subexplength);
 					nodes.Add(getArithmaticNode(op));
@@ -49,7 +49,7 @@ namespace ExpressionEval {
 		}
 
 		public static Node convergeOperators(List<Node> nodes) {
-			if (nodes.Count == 1 && nodes[0] is ConstantNode)
+			if (nodes.Count == 1)
 				return nodes[0];
 
 			convergeOperator<DivideNode>	(nodes);
@@ -73,18 +73,9 @@ namespace ExpressionEval {
 				if (i != 0) {
 					@operator.left = nodes[i - 1];
 					nodes.RemoveAt(i - 1);
+					i--;
 				}
-
-				i--;
 			}
-		}
-
-		private static ArithmeticNode buildArithmeticNode(Node left, Node right, char op) {
-			var @operator = getArithmaticNode(op);
-			@operator.left = left;
-			@operator.right = right;
-
-			return @operator;
 		}
 
 		private static ArithmeticNode getArithmaticNode(char op) {
@@ -102,18 +93,6 @@ namespace ExpressionEval {
 			return null;
 		}
 
-		private static int findCloseBracket(int index, string expression) {
-			int depth = 0;
-			do {
-				if (expression[index] == '(')
-					depth++;
-				else if (expression[index] == ')')
-					depth--;
-			} while (index < expression.Length && depth > 0);
-
-			return index;
-		}
-
 		private static int findEndOfNumber(int startindex, string expression) {
 			while (startindex < expression.Length && expression[startindex].isNumber())
 				startindex++;
@@ -121,12 +100,8 @@ namespace ExpressionEval {
 			return startindex;
 		}
 
-		private static bool isNumber(this char character) {
-			return character >= '0' && character <= '9';
-		}
+		private static bool isNumber(this char character) => character >= '0' && character <= '9';
 
-		private static bool isOperator(this char character) {
-			return operators.Any(c => c == character);
-		}
+		private static bool isOperator(this char character) => operators.Any(c => c == character);
 	}
 }
